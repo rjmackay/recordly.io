b48859f121cf618948f8628f2c2b9e9c47b38579
Robbie Mackay <rm@robbiemackay.com>
2014-07-19 15:15:50 +1200

    Gulpfile: add livereload, just beep/log on watch errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,41 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	livereload = require('gulp-livereload'),
+	plumber = require('gulp-plumber'),
+	gutil = require('gulp-util');
+
+function errorHandler (err) {
+  gutil.beep();
+  gutil.log(err.message || err);
+}
 
 gulp.task('default', function() {
-  // place code for your default task here
+	// place code for your default task here
 });
 
-var sass = require('gulp-sass');
 gulp.task('sass', function () {
-    gulp.src('./public/scss/*.scss')
-        .pipe(sass({
-        	includePaths : [
-        		'public/bower_components/bourbon/dist',
-        		'public/bower_components/neat/app/assets/stylesheets',
-        		'public/bower_components/font-awesome/scss'
-        	],
-        	outputStyle : 'nested'
-        }))
-        .pipe(gulp.dest('./public/css'));
+	gulp.src('./public/scss/*.scss')
+		.pipe(plumber({
+			errorHandler: errorHandler
+		}))
+		.pipe(sass({
+			includePaths : [
+				'public/bower_components/bourbon/dist',
+				'public/bower_components/neat/app/assets/stylesheets',
+				'public/bower_components/font-awesome/scss'
+			],
+			outputStyle : 'nested'
+		}))
+		.pipe(plumber.stop())
+		.pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('watch', function(){
-    gulp.watch('./public/scss/**/*.scss', ['sass']);
+	livereload.listen();
+	gulp.watch('./public/scss/**/*.scss', ['sass'])
+		.on('change', livereload.changed);
 });
 
 gulp.task('build', ['sass'], function() {
-  // place code for your default task here
+	// place code for your default task here
 });
