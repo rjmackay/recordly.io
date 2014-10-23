c0fe05cec0d01889fd75979fd9625d317ee5c41c
Robbie Mackay <rm@robbiemackay.com>
2014-10-23 13:46:43 +1300

    Add browserify set up

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,21 @@ var gulp = require('gulp'),
 	livereload = require('gulp-livereload'),
 	plumber = require('gulp-plumber'),
 	gutil = require('gulp-util'),
-	jshint = require('gulp-jshint');
+	jshint = require('gulp-jshint'),
+	source = require('vinyl-source-stream'),
+	browserify = require('browserify'),
+	watchify = require('watchify'),
+
+	browserifyConfig = {
+		entries : './client/js/app.js',
+		debug : true
+	}
+;
 
 function errorHandler (err) {
   gutil.beep();
   gutil.log(err.message || err);
 }
-
-gulp.task('default', function() {
-	// place code for your default task here
-});
 
 gulp.task('sass', function () {
 	gulp.src('./public/scss/*.scss')
@@ -31,20 +36,53 @@ gulp.task('sass', function () {
 		.pipe(gulp.dest('./public/css'));
 });
 
+/**
+ * Task: `browserify`
+ * Bundle js with browserify
+ */
+gulp.task('browserify', function() {
+    browserify(browserifyConfig)
+        .transform('brfs')
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('./public/js'));
+});
+
+/**
+ * Task: `watchify`
+ * Watch js and rebundle with browserify
+ */
+gulp.task('watchify', function() {
+    var bundler = watchify(browserify(browserifyConfig, watchify.args))
+    .transform('brfs')
+    .on('update', rebundle);
+
+    function rebundle () {
+        return bundler.bundle()
+            .on('error', errorHandler)
+            .pipe(source('bundle.js'))
+            .pipe(gulp.dest('public/js'));
+    }
+});
+
 gulp.task('lint', function() {
-	gulp.src(['./public/js/main.js', './*.js'])
+	gulp.src(['./public/js/main.js', 'client/js/**/*.js', './*.js'])
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'));
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', ['watchify'], function(){
 	livereload.listen();
 	gulp.watch('./public/scss/**/*.scss', ['sass']);
-	gulp.watch(['./public/js/main.js', './*.js'], ['lint']);
+	gulp.watch(['./public/js/main.js', 'client/js/**/*.js', './*.js'], ['lint']);
 	gulp.watch(['./public/*.html', './public/js/*.js', './public/css/*.css'])
 		.on('change', livereload.changed);
 });
 
-gulp.task('build', ['sass'], function() {
+gulp.task('default', function() {
+	// place code for your default task here
+});
+
+gulp.task('build', ['sass', 'browserify'], function() {
 	// place code for your default task here
 });
